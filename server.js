05e17a72a454d96354dc6c17cdb70b7751596aec
Volnei Rodrigues <volneir.designer@gmail.com>
2025-07-03 17:08:00 -0300

    Fix conexão e dns

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+// CORREÇÃO: Use 'require' em vez de 'import'
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
+
 require('dotenv').config();
 const express = require('express');
 const { Pool } = require('pg');
